380079953952cdfdfaa56b7ac8e7f328947d29ec
FAHIM-MIM <fahimfaisal562@gmail.com>
2021-07-03 23:16:47 +0600

    first commit

---
+++ dashboard.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Night Mart</title>
+<style>
+        body{
+          margin: auto;
+          width: 30%;
+          padding: 20px;
+
+        }
+        .make-it-center{
+          margin: auto;
+          width: 50%;
+        }
+        .error{
+        	color: red;
+        }
+        .required:after {
+          content:"*";
+          color: red;
+        }
+        .sidenav {
+            height: "auto";
+            position: fixed;
+            top: 0; 
+            left: 0;
+            padding-left: 200px;
+            padding-top: 80px;
+        }
+
+        .sidenav a {
+            padding: 6px 8px 6px 16px;
+            text-decoration: none;
+            display: block;
+        }
+        .footer{
+            text-align:center;
+        }
+    </style>
+</head>
+
+<body>
+
+<?php
+include 'templates/sidenav.php';
+session_start();
+include 'templates/header.php';
+$userErr = $passErr = "";
+$username = $password = "";
+$errCount = 0;
+
+if (isset($_SESSION['uname'])) {
+    echo "<h1> Welcome ".$_SESSION['uname']." to the Night Mart</h1>";
+
+} else{
+    header('Location: login.php');
+}
+
+?>
+<br>
+</body>
+<?php include 'templates/footer.php';?>
+</html>
